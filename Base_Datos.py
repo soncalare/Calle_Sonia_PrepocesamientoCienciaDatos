d00b4d8c38279c41aadb95189d287eff911db623
soncalare <soncalare10@gmail.com>
2025-05-14 14:50:06 -0500

    Agrega Base_Datos.py con script para leer archivo Excel

---
+++ Base_Datos.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Ruta del archivo
+ruta = r"C:\Users\ACER\Desktop\unach\MODELAMIENTO\script R\DISEÑO EXPERIMENTAL\DISEÑO EXPERIMENTAL\Bloques.xlsx"
+
+# Leer el archivo Excel
+df = pd.read_excel(C:\Users\ACER\Desktop\unach\MODELAMIENTO\script R\DISEÑO EXPERIMENTAL\DISEÑO EXPERIMENTAL\Bloques.xlsx", sheet_name="Hoja 3")
+
+# Mostrar las primeras filas
+print(df.head())
